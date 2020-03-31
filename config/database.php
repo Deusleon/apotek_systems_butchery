64a6d002e609ccc3ca600c9aa4d3c0d5914f1fe4
maricajr <maricaneston38@gmail.com>
2020-03-31 10:47:30 +0300

    sandr database added

--- config/database.php
+++ config/database.php
@@ -134,6 +134,28 @@ return [
         ],
         /*fahari end*/
 
+        /*sandr*/
+        'sandr' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'sandr',
+            'username' => 'root',
+            'password' => 'Apotek@dba1',
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => false,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+        /*fahari end*/
+
         /*online only end*/
 
         /*offline only*/
