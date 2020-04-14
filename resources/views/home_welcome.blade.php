6eda1881134f8bf9843cb2f3520352cb13e9e623
maricajr <maricaneston38@gmail.com>
2020-04-14 15:21:46 +0300

    dashboard icon alignment

--- resources/views/home_welcome.blade.php
+++ resources/views/home_welcome.blade.php
@@ -9,52 +9,58 @@
     <div class="col-xl-12">
         <div class="card-columns">
             <div class="card" id="zoom">
-                <img style="margin-left: auto; margin-right: auto; display: block; width: 35%;padding: 10px;"
-                     src="{{asset('img/coupon.png')}}" alt="Card image cap">
-                <div class="card-body">
+                <img
+                    style="margin-left: auto; margin-right: auto; margin-top: 10%; display: block; width: 35%;padding: 10px;"
+                    src="{{asset('img/coupon.png')}}" alt="Card image cap">
+                <div class="card-body" style="margin-top: -10%">
                     <h5 class="card-title">Sales</h5>
                     <p class="card-text">Sales Module enables you to manage cash & credit sales, quotes, payments.</p>
                 </div>
             </div>
             <div class="card" id="zoom">
-                <img style="margin-left: auto; margin-right: auto; display: block; width: 35%;padding: 10px;"
-                     src="{{asset('img/inventory.png')}}" alt="Card image cap">
-                <div class="card-body">
+                <img
+                    style="margin-left: auto; margin-right: auto; margin-top: 10%; display: block; width: 35%;padding: 10px;"
+                    src="{{asset('img/inventory.png')}}" alt="Card image cap">
+                <div class="card-body" style="margin-top: -10%">
                     <h5 class="card-title">Inventory</h5>
                     <p class="card-text">Inventory Module handles stock quantities, prices, stock adjustment, stock
                         transfer.</p>
                 </div>
             </div>
             <div class="card" id="zoom">
-                <img style="margin-left: auto; margin-right: auto; display: block; width: 35%;padding: 10px;"
-                     src="{{asset('img/cart.png')}}" alt="Card image cap">
-                <div class="card-body">
+                <img
+                    style="margin-left: auto; margin-right: auto; margin-top: 10%; display: block; width: 35%;padding: 10px;"
+                    src="{{asset('img/cart.png')}}" alt="Card image cap">
+                <div class="card-body" style="margin-top: -10%">
                     <h5 class="card-title">Purchases</h5>
                     <p class="card-text">Purchases Module manages purchases orders, goods receiving, supplier
                         invoices.</p>
                 </div>
             </div>
             <div class="card" id="zoom">
-                <img style="margin-left: auto; margin-right: auto; display: block; width: 35%;padding: 10px;"
-                     src="{{asset('img/expenses.png')}}" alt="Card image cap">
-                <div class="card-body">
+                <img
+                    style="margin-left: auto; margin-right: auto; margin-top: 10%; display: block; width: 35%;padding: 10px;"
+                    src="{{asset('img/expenses.png')}}" alt="Card image cap">
+                <div class="card-body" style="margin-top: -10%">
                     <h5 class="card-title">Expenses</h5>
                     <p class="card-text">Expenses Module track and report all expenses associated with the business.</p>
                 </div>
             </div>
             <div class="card" id="zoom">
-                <img style="margin-left: auto; margin-right: auto; display: block; width: 35%;padding: 10px;"
-                     src="{{asset('img/report.png')}}" alt="Card image cap">
-                <div class="card-body">
+                <img
+                    style="margin-left: auto; margin-right: auto; margin-top: 10%; display: block; width: 35%;padding: 10px;"
+                    src="{{asset('img/report.png')}}" alt="Card image cap">
+                <div class="card-body" style="margin-top: -10%">
                     <h5 class="card-title">Reports</h5>
                     <p class="card-text">Reports Module generates various kinds of reports for data visualization &
                         analytics.</p>
                 </div>
             </div>
             <div class="card" id="zoom">
-                <img style="margin-left: auto; margin-right: auto; display: block; width: 35%;padding: 10px;"
-                     src="{{asset('img/list.png')}}" alt="Card image cap">
-                <div class="card-body">
+                <img
+                    style="margin-left: auto; margin-right: auto; margin-top: 10%; display: block; width: 35%;padding: 10px;"
+                    src="{{asset('img/list.png')}}" alt="Card image cap">
+                <div class="card-body" style="margin-top: -10%">
                     <h5 class="card-title">Masters</h5>
                     <p class="card-text">Masters Module manages products, categories, suppliers, stores, locations.</p>
                 </div>
