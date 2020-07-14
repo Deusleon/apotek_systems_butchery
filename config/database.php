c6e67f676b4d463ab9426b0bc08a9616df4dbe72
Neston Marica <maricaneston38@gmail.com>
2020-07-14 10:30:06 +0000

    database.php edited online with Bitbucket (eternity account added)

--- config/database.php
+++ config/database.php
@@ -243,6 +243,28 @@ return [
             ]) : [],
         ],
         /*roia end*/
+        
+        /*eternity*/
+        'eternity' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'eternity',
+            'username' => 'root',
+            'password' => 'Apotek@dba1',
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => false,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+        /*eternity end*/
 
         /*online only end*/
 
