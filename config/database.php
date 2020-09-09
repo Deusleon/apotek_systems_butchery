05361dff0519ab56e8ad10ea6f3c72599defff13
Stanley Gama <stansgma@gmail.com>
2020-09-09 16:10:39 +0300

    Added mbokomu and maina client databases

--- config/database.php
+++ config/database.php
@@ -266,6 +266,50 @@ return [
         ],
         /*eternity end*/
 
+        /*mbokomu*/
+        'mbokomu' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'mbokomu',
+            'username' => 'root',
+            'password' => 'Apotek@dba1',
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => false,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+        /*mbokomu end*/
+
+        /*maina*/
+        'maina' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'maina',
+            'username' => 'root',
+            'password' => 'Apotek@dba1',
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => false,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+        /*maina end*/
+
         /*online only end*/
 
         /*offline only*/
