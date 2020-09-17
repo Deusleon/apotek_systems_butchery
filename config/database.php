9a01347db661c4db6ef563d69228fd1ab94b9f1a
Stanley Gama <stansgma@gmail.com>
2020-09-17 10:58:24 +0300

    Added godwin buguruni connection

--- config/database.php
+++ config/database.php
@@ -309,6 +309,27 @@ return [
             ]) : [],
         ],
         /*maina end*/
+        /*godwin_buguruni */
+        'godwin_buguruni' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'godwin_buguruni',
+            'username' => 'root',
+            'password' => 'Apotek@dba1',
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => false,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+        /*godwin_buguruni end*/
 
         /*online only end*/
 
