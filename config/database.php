4f48f93e28192f57308495c8af65910dc7b946dc
Stanley Gama <stansgma@gmail.com>
2020-10-06 11:46:47 +0300

    Added artfor customer

--- config/database.php
+++ config/database.php
@@ -331,6 +331,28 @@ return [
         ],
         /*godwin_buguruni end*/
 
+        /*Artfor*/
+        'artfor' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'artfor',
+            'username' => 'root',
+            'password' => 'Apotek@dba1',
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => false,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+        /*Artfor end*/
+
         /*online only end*/
 
         /*offline only*/
