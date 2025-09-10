58790995b1cac6108577bafffbb8762e49e88f4f
deusleon7 <deusleon7@gmail.com>
2025-09-10 17:09:24 +0300

    Cash sales details report developed

--- resources/views/sale_reports/cash_sale_detail_report1_pdf.blade.php
+++ resources/views/sale_reports/cash_sale_detail_report1_pdf.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Cash Sale Details Report</title>
     <style>
-
         body {
             font-size: 12px;
         }
@@ -12,7 +12,9 @@
             font-family: Verdana, Arial, sans-serif;
         }
 
-        table, th, td {
+        table,
+        th,
+        td {
             /*border: 1px solid black;*/
             border-collapse: collapse;
             padding: 10px;
@@ -58,7 +60,7 @@
             border-collapse: collapse;
         }
 
-        #table-detail tr > {
+        #table-detail tr> {
             line-height: 13px;
         }
 
@@ -88,144 +90,151 @@
             max-width: 160px;
             max-height: 160px;
         }
-
-
     </style>
 </head>
+
 <body>
-<div class="row">
-    <div id="container">
-        <div class="logo-container">
-            @if($pharmacy['logo'])
-                <img src="{{public_path('fileStore/logo/'.$pharmacy['logo'])}}"/>
-            @endif
+    <div class="row">
+        <div id="container">
+            <div class="logo-container">
+                @if($pharmacy['logo'])
+                    <img src="{{public_path('fileStore/logo/' . $pharmacy['logo'])}}" />
+                @endif
+            </div>
         </div>
     </div>
-</div>
-<div class="row" style="padding-top: -2%">
-    <h1 align="center">{{$pharmacy['name']}}</h1>
-    <h3 align="center" style="margin-top: -1%">{{$pharmacy['address']}}</h3>
-    <h3 align="center" style="margin-top: -1%">{{$pharmacy['phone']}}</h3>
-    <h3 align="center" style="margin-top: -1%">{{$pharmacy['email'].' | '.$pharmacy['website']}}</h3>
-    <h2 align="center" style="margin-top: -1%">Cash Sales Details Report</h2>
-    <h4 align="center" style="margin-top: -1%">{{$pharmacy['date_range']}}</h4>
-    @foreach($data[0][0] as $key => $dat)
-        {{--        {{$pharmacy['tin_number']}} {{date('j M, Y', strtotime($dat[0]['created_at']))}}--}}
-        <table id="table-detail-main">
-            <tr>
-                <td><b>Date:</b> {{date('j M, Y', strtotime($dat[0]['created_at']))}}</td>
-            </tr>
-        </table>
-        <table id="table-detail" align="center">
-            <!-- loop the product names here -->
-            <thead>
-            <tr style="background: #1f273b; color: white;">
-                <th align="left">SN</th>
-                <th align="left">Product Name</th>
-                <th align="right">Quantity</th>
-                <th align="right">Price</th>
-                {{--                <th align="right">Sub Total</th>--}}
-                {{--                <th align="right">Discount</th>--}}
-                <th align="right">VAT</th>
-                <th align="right">Amount</th>
-            </tr>
-            </thead>
-            @foreach($dat as $item)
+    <div class="row" style="padding-top: -2%">
+        <h1 align="center">{{$pharmacy['name']}}</h1>
+        <h3 align="center" style="margin-top: -1%">{{$pharmacy['address']}}</h3>
+        <h3 align="center" style="margin-top: -1%">{{$pharmacy['phone']}}</h3>
+        <h3 align="center" style="margin-top: -1%">{{$pharmacy['email'] . ' | ' . $pharmacy['website']}}</h3>
+        <h2 align="center" style="margin-top: -1%">Cash Sales Details Report</h2>
+        <h4 align="center" style="margin-top: -1%">{{$pharmacy['date_range']}}</h4>
+        @foreach($data[0][0] as $key => $dat)
+            {{-- {{$pharmacy['tin_number']}} {{date('j M, Y', strtotime($dat[0]['created_at']))}}--}}
+            <table id="table-detail-main">
                 <tr>
-                    <td align="left">{{$loop->iteration}}</td>
-                    <td align="left">{{$item['name']}}</td>
-                    <td align="right">{{number_format($item['quantity'],0)}}</td>
-                    <td align="right">{{number_format($item['price']/$item['quantity'],2)}}</td>
-                    {{--                    <td align="right">{{number_format($item['sub_total'],2)}}</td>--}}
-                    {{--                    <td align="right">{{number_format($item['discount'],2)}}</td>--}}
-                    <td align="right">{{number_format($item['vat'],2)}}</td>
-                    <td align="right">{{number_format($item['amount'],2)}}</td>
-                </tr>
-            @endforeach
-        </table>
-
-        @foreach($data[0][2][$key] as $e)
-            {{--            {{number_format($e['test_total'],2)}}--}}
-            <table style="width: 103%; background-color: white">
-                <thead>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                </thead>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td align="right" style="padding-top: -3%"><b>Sub Total:</b></td>
-                    <td align="right"
-                        style="padding-top: -3%">{{number_format($e['amount_total']-$e['vat_total']+$e['discount_total'],2)}}</td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td align="right" style="padding-top: -3%"><b>Discount:</b></td>
-                    <td align="right" style="padding-top: -3%">{{number_format($e['discount_total'],2)}}</td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td align="right" style="padding-top: -3%"><b>VAT:</b></td>
-                    <td align="right" style="padding-top: -3%">{{number_format($e['vat_total'],2)}}</td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td align="right" style="padding-top: -3%"><b>Total:</b></td>
-                    <td align="right" style="padding-top: -3%">{{number_format($e['amount_total'],2)}}</td>
+                    <td><b>Date:</b> {{date('j M, Y', strtotime($dat[0]['created_at']))}}</td>
                 </tr>
             </table>
+            <table id="table-detail" align="center">
+                <!-- loop the product names here -->
+                <thead>
+                    <tr style="background: #1f273b; color: white;">
+                        <th align="left">SN</th>
+                        <th align="left">Product Name</th>
+                        <th align="left">Batch Number</th>
+                        <th align="left">Price Type</th>
+                        <th align="left">Sold By</th>
+                        <th align="left">Date</th>
+                        <th align="right">Quantity</th>
+                        <th align="right">Sell Price</th>
+                        <th align="right">Sub total</th>
+                        <th align="right">VAT</th>
+                        <th align="right">Discount</th>
+                        <th align="right">Amount</th>
+                    </tr>
+                </thead>
+                @foreach($dat as $item)
+                    <tr>
+                        <td align="left">{{$loop->iteration}}</td>
+                        <td align="left">{{$item['name']}}</td>
+                        <td align="left">{{$item['batch_number']}}</td>
+                        <td align="left">{{$item['price_type']}}</td>
+                        <td align="left">{{$item['sold_by']}}</td>
+                        <td align="left">{{$item['date']}}</td>
+                        <td align="right">{{number_format($item['quantity'], 0)}}</td>
+                        <td align="right">{{number_format($item['price'], 2)}}</td>
+                        <td align="right">{{number_format($item['sub_total'], 2)}}</td>
+                        <td align="right">{{number_format($item['vat'], 2)}}</td>
+                        <td align="right">{{number_format($item['discount'], 2)}}</td>
+                        <td align="right">{{number_format($item['amount'], 2)}}</td>
+                    </tr>
+                @endforeach
+            </table>
+
+            @foreach($data[0][2][$key] as $e)
+                {{-- {{number_format($e['test_total'],2)}}--}}
+                <table style="width: 103%; background-color: white">
+                    <thead>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </thead>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td align="right" style="padding-top: -3%"><b>Sub Total:</b></td>
+                        <td align="right" style="padding-top: -3%">
+                            {{number_format($e['amount_total'] - $e['vat_total'] + $e['discount_total'], 2)}}</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td align="right" style="padding-top: -3%"><b>Discount:</b></td>
+                        <td align="right" style="padding-top: -3%">{{number_format($e['discount_total'], 2)}}</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td align="right" style="padding-top: -3%"><b>VAT:</b></td>
+                        <td align="right" style="padding-top: -3%">{{number_format($e['vat_total'], 2)}}</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td align="right" style="padding-top: -3%"><b>Total:</b></td>
+                        <td align="right" style="padding-top: -3%">{{number_format($e['amount_total'], 2)}}</td>
+                    </tr>
+                </table>
+            @endforeach
+
+
+
+            <hr>
         @endforeach
 
+    </div>
 
-
-        <hr>
-    @endforeach
-
-</div>
-
-<script type="text/php">
+    <script type="text/php">
     if ( isset($pdf) ) {
         $x = 280;
         $y = 820;
@@ -248,4 +257,3 @@
 </body>
 
 </html>
-
