60b7f3969fa9f062fe170c000928f308f6c3974e
deusleon7 <deusleon7@gmail.com>
2025-10-16 14:48:25 +0300

    Bug fixing, on cash sales in ALL branch

--- resources/views/stock_management/adjustments/adjust_stock_modal.blade.php
+++ resources/views/stock_management/adjustments/adjust_stock_modal.blade.php
@@ -59,7 +59,7 @@
 
 <!-- Confirmation Modal -->
 <div class="modal fade" id="confirmAdjustmentModal" tabindex="-1" role="dialog" aria-labelledby="confirmAdjustmentModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-sm" role="document">
+  <div class="modal-dialog modal-md" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="confirmAdjustmentModalLabel">Confirm Adjustment</h5>
